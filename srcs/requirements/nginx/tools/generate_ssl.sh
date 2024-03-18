a202a60f629df7be27ef46c6ea7a591f52392e66
hafid saadi <iflis@hsaadi.hsaadi>
2024-03-17 20:01:05 -0400

    adding mariadb support

--- srcs/requirements/nginx/tools/generate_ssl.sh
+++ srcs/requirements/nginx/tools/generate_ssl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
- 
-if [ "$#" -ne 1 ]
+echo "Arguments received: $@" 
+
+if [ "$#" -ne 1 ];
 then
   echo "Usage: Must supply a domain"
   exit 1
@@ -9,18 +10,22 @@ fi
 SSLPATH=/etc/ssl
 KEYPATH=/etc/ssl/private
 CERTPATH=/etc/ssl/certs
-mkdir -p "$KEYPATH" "$CERTPATH"
+#mkdir -p "$KEYPATH" "$CERTPATH"
 
 DOMAIN=$1
 
-openssl req -x509 -newkey rsa:2048 -nodes -keyout $KEYPATH/myCA.key -sha256 -days 365 -out $CERTPATH/myCA.pem -subj "/C=CA/ST=QUEBEC/L=QUEBEC/O=42QUEBEC/OU=hsaadi/CN=hsaadi.42.fr" 
+openssl req  -x509 -newkey rsa:2048 -nodes -keyout $KEYPATH/myCA.key -sha256 -days 365 -out $SSLPATH/myCA.pem -subj "/C=CA/ST=QUEBEC/L=QUEBEC/O=42QUEBEC/OU=hsaadi/CN=hsaadi.42.fr" 
 
-cp $CERTPATH/myCA.pem /usr/share/ca-certificates/myCA.crt
+cp $SSLPATH/myCA.pem /usr/share/ca-certificates/myCA.crt
 
 # Update the certificates
 apk update update-ca-certificates
 
-openssl req -newkey rsa:2048 -nodes -keyout $KEYPATH/$DOMAIN.key -out $CERTPATH/$DOMAIN.csr
+openssl genrsa -out $KEYPATH/$DOMAIN.key 2049
+openssl req -new -key $KEYPATH/$DOMAIN.key -out $CERTPATH/$DOMAIN.csr -subj "/C=CA"
+#openssl req -newkey rsa:2048 -nodes -keyout $KEYPATH/$DOMAIN.key -out $CERTPATH/$DOMAIN.csr
+
+#chmod +w 
 
 cat > $CERTPATH/$DOMAIN.ext << EOF
 authorityKeyIdentifier=keyid,issuer
@@ -33,6 +38,4 @@ EOF
 
 openssl x509 -req -in $CERTPATH/$DOMAIN.csr -CA $SSLPATH/myCA.pem -CAkey $KEYPATH/myCA.key -CAcreateserial \
 -out $CERTPATH/$DOMAIN.crt -days 365 -sha256 -extfile $CERTPATH/$DOMAIN.ext
-
-echo "***************** THE END **********************"
 
